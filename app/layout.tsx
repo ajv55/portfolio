5b02eb1efbe05dbf0cdf8afe3191ffe2dcf2d6c1
ajv55 <118780510+ajv55@users.noreply.github.com>
2024-09-18 16:44:32 -0400

    added opengraph image and some basic seo like roboto file and sitemap file

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,19 @@ const roboto = Roboto({weight: '400', subsets: ["latin"]})
 
 export const metadata: Metadata = {
   title: "Portfolio | Abel Villareal - Web Developer",
-  description: "Welcome to Abel Villareal's portfolio. A self-taught web developer proficient in JavaScript, TypeScript, React, Next.js, and more. Explore my projects, skills, and experience in building modern web applications with animations, responsive design, and dynamic features.",
+  description:
+    "Welcome to Abel Villareal's portfolio. A self-taught web developer proficient in JavaScript, TypeScript, React, Next.js, and more. Explore my projects, skills, and experience in building modern web applications with animations, responsive design, and dynamic features.",
+  openGraph: {
+    title: "Portfolio | Abel Villareal - Web Developer",
+    description:
+      "Discover Abel Villareal's portfolio, showcasing projects built with JavaScript, React, Next.js, and more. Explore my experience as a web developer.",
+    url: "https://www.abelj.dev/",
+    siteName: "Abel Villareal Portfolio",
+    locale: "en_US",
+    type: "website",
+  },
 };
+
 
 
 export default function RootLayout({
